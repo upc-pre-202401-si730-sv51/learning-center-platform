209380da5cc3c1549593c997b3a31736ad591e94
Angel Velasquez <pcisavel@upc.edu.pe>
2024-05-27 10:35:31 -0500

    feat(publishing): added relationship in category side.

--- ACME.LearningCenterPlatform.API/Publishing/Domain/Model/Entities/Category.cs
+++ ACME.LearningCenterPlatform.API/Publishing/Domain/Model/Entities/Category.cs
@@ -1,3 +1,5 @@
+using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Aggregates;
+
 namespace ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Entities;
 
 public class Category(string name)
@@ -8,4 +10,6 @@ public class Category(string name)
     public Category() : this(string.Empty)
     {
     }
+    
+    public ICollection<Tutorial> Tutorials { get; } 
 }
