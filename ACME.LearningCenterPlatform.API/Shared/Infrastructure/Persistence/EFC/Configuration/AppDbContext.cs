f0d0fc49a3ab9536528d626221cd1b45323c78a9
Angel Velasquez <pcisavel@upc.edu.pe>
2024-05-27 10:51:40 -0500

    feat(publishing): implemented object-relational mapping rules.

--- ACME.LearningCenterPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ ACME.LearningCenterPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,3 +1,5 @@
+using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Aggregates;
+using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Entities;
 using ACME.LearningCenterPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
 using Microsoft.EntityFrameworkCore;
@@ -17,7 +19,43 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     {
         base.OnModelCreating(builder);
         
-        // Place here the configuration of the entities
+        // Publishing Context
+        
+        builder.Entity<Category>().HasKey(c => c.Id);
+        builder.Entity<Category>().Property(c => c.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Category>().Property(c => c.Name).IsRequired().HasMaxLength(30);
+        
+        builder.Entity<Tutorial>().HasKey(t => t.Id);
+        builder.Entity<Tutorial>().Property(t => t.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Tutorial>().Property(t => t.Title).IsRequired().HasMaxLength(50);
+        builder.Entity<Tutorial>().Property(t => t.Summary).HasMaxLength(240);
+
+        builder.Entity<Asset>().HasDiscriminator(a => a.Type);
+        builder.Entity<Asset>().HasKey(a => a.Id);
+        builder.Entity<Asset>().Property(a => a.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Asset>().HasDiscriminator<string>("asset_type")
+            .HasValue<Asset>("asset_base")
+            .HasValue<ImageAsset>("asset_image")
+            .HasValue<VideoAsset>("asset_video")
+            .HasValue<ReadableContentAsset>("asset_readable_content");
+
+        builder.Entity<Asset>().OwnsOne(i => i.AssetIdentifier,
+            ai =>
+            {
+                ai.WithOwner().HasForeignKey("Id");
+                ai.Property(p => p.Identifier).HasColumnName("AssetIdentifier");
+            });
+
+        builder.Entity<ImageAsset>().Property(p => p.ImageUri).IsRequired();
+        builder.Entity<VideoAsset>().Property(p => p.VideoUri).IsRequired();
+        builder.Entity<ReadableContentAsset>().Property(p => p.ReadableContent).IsRequired();
+
+        builder.Entity<Tutorial>().HasMany(t => t.Assets);
+
+        builder.Entity<Category>().HasMany(c => c.Tutorials)
+            .WithOne(t => t.Category)
+            .HasForeignKey(t => t.CategoryId)
+            .HasPrincipalKey(c => c.Id);
         
         // Apply SnakeCase Naming Convention
         builder.UseSnakeCaseWithPluralizedTableNamingConvention();
